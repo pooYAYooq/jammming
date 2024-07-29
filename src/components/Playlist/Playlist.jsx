a4c5ab04b239884df719e12897ca0d051cba8c7f
pooYAYooq <pooyayooq@gmail.com>
2024-07-30 01:03:07 +1000

    update Playlist component className

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -3,13 +3,13 @@ import "./Playlist.css";
 
 function Playlist({ playlistName, onNameChange, onSave }) {
   return (
-    <div className="Playlist">
+    <div className="playlist">
       <input 
         value={playlistName}
         onChange={onNameChange}
         placeholder="New Playlist"
       />
-      <button className="Playlist-save" onClick={onSave}>
+      <button className="playlist-save" onClick={onSave}>
         SAVE TO SPOTIFY
       </button>
       {/* We'll add the TrackList component here later */}
